28af2408f86910c2313fa9b010c72c626ede1345
alaaAbdelrahman <a.abdelrahman1399@gmail.com>
2024-04-14 22:56:30 +0200

    setup for the game

---
+++ build/LightYear/CMakeFiles/LIGHT_YEARS_GAME_TARGET_NAME.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/LIGHT_YEARS_GAME_TARGET_NAME"
+  "../bin/LIGHT_YEARS_GAME_TARGET_NAME.pdb"
+  "CMakeFiles/LIGHT_YEARS_GAME_TARGET_NAME.dir/src/main.cpp.o"
+  "CMakeFiles/LIGHT_YEARS_GAME_TARGET_NAME.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIGHT_YEARS_GAME_TARGET_NAME.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/LightYear/CMakeFiles/LIGHT_YEARS_GAME_TARGET_NAME.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LIGHT_YEARS_GAME_TARGET_NAME.
+# This may be replaced when dependencies are built.
